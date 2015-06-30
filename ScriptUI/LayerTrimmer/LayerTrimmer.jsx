ceb071c3533d85b46d21f9531358d53b91271225
kareobana <kareobana@atarabi.com>
2015-06-30 23:51:19 +0900

    Update LayerTrimmer

--- ScriptUI/LayerTrimmer/LayerTrimmer.jsx
+++ ScriptUI/LayerTrimmer/LayerTrimmer.jsx
@@ -4,21 +4,28 @@
  *  Author: Kareobana(http://atarabi.com/)
  *  License: MIT
  *  Dependencies:
+ *    KIKAKU.JSON
  *    KIKAKU.Utils 1.0.0
  *    KIKAKU.UIBuilder 2.0.0
  */
 
 (function (global) {
   //Lib
-  var  Utils = KIKAKU.Utils,
+  var JSON = KIKAKU.JSON,
+    Utils = KIKAKU.Utils,
     UIBuilder = KIKAKU.UIBuilder;
     
   //Utility
-  var isTextLayer = Utils.isTextLayer,
+  var assign = Utils.assign,
+    forEach = Utils.forEach,
+    isTextLayer = Utils.isTextLayer,
     isShapeLayer = Utils.isShapeLayer,
+    isCompLayer = Utils.isCompLayer,
     getActiveComp = Utils.getActiveComp,
     getCompByName = Utils.getCompByName,
-    getSelectedLayer = Utils.getSelectedLayer;
+    getSelectedLayer = Utils.getSelectedLayer,
+    getLayer = Utils.getLayer,
+    getLayers = Utils.getLayers;
     
   //Constants
   var SCAN = {
@@ -47,90 +54,157 @@
       property.setValue(value);
     }
   }
+  
+  function setComment(layer, obj) {
+    var comment = layer.comment;
+    try {
+      comment = JSON.parse(comment);
+    } catch (e) {
+      comment = comment ? {comment: comment} : {};
+    }
+    assign(comment, obj);
+
+    layer.comment = JSON.stringify(comment);
+  }
+
+  function getComment(layer) {
+    var comment = layer.comment;
+    try {
+      comment = JSON.parse(comment);
+    } catch (e) {
+      comment = {
+        comment: comment
+      };
+    }
+    return comment;
+  }
+
+  function removeComment(layer, key) {
+    var comment = layer.comment;
+    try {
+      comment = JSON.parse(comment);
+    } catch (e) {
+      return;
+    }
+  
+    delete comment[key];
+
+    var has_key = false,
+      only_comment = true;
+    for (var k in comment) {
+      has_key = true;
+      if (k !== 'comment') {
+        only_comment = false;
+        break;
+      }
+    }
+  
+    if (!has_key) {
+      layer.comment = '';
+    } else {
+      layer.comment = only_comment ? comment.comment : JSON.stringify(comment);
+    }
+  }
+  
+  function getRect(comp, layer, scan, extents) {
+    var rect = {
+      left: Infinity,
+      top: Infinity,
+      right: -Infinity,
+      bottom: -Infinity,
+    };
+    
+    if (scan === SCAN.CURRENT_FRAME) {
+      var _rect = layer.sourceRectAtTime(layer.time, extents);
+      rect.left = _rect.left;
+      rect.top = _rect.top;
+      rect.right = _rect.left + _rect.width;
+      rect.bottom = _rect.top + _rect.height;
+    } else if (scan === SCAN.ALL_FRAMES) {
+      var in_point = layer.inPoint,
+        out_point = layer.outPoint,
+        frame_duration = comp.frameDuration;
+        
+      if (in_point > out_point) {
+        var temp = in_point;
+        in_point = out_point;
+        out_point = temp;
+      }
+      
+      for (var t = in_point; t < out_point; t += frame_duration) {
+        var _rect = layer.sourceRectAtTime(t, extents);
+        rect.left = Math.min(rect.left, _rect.left);
+        rect.top = Math.min(rect.top, _rect.top);
+        rect.right = Math.max(rect.right, _rect.left + _rect.width);
+        rect.bottom = Math.max(rect.bottom, _rect.top + _rect.height);
+      }
+    }
+    
+    return rect;
+  }
+  
+  function createPrecomp(script_name, comp, layer, name, rect, center, padding, precomp) {
+    var width,
+      height,
+      position,
+      pixel_aspect = comp.pixelAspect,
+      duration = Math.abs(layer.inPoint - layer.outPoint),
+      frame_rate = comp.frameRate;
+      
+    if (center === CENTER.COMPOSITION) {
+      width = 2 * (Math.ceil(Math.max(Math.abs(rect.left), Math.abs(rect.right))) + padding);
+      height = 2 * (Math.ceil(Math.max(Math.abs(rect.top), Math.abs(rect.bottom))) + padding);
+      position = [width / 2, height / 2];
+    } else if (center === CENTER.CONTENTS) {
+      width = Math.ceil(rect.right - rect.left) + 2 * padding;
+      height = Math.ceil(rect.bottom - rect.top) + 2 * padding;
+      position = [width / 2 - (rect.left + rect.right) / 2, height / 2 - (rect.top + rect.bottom) / 2];
+    }
+    
+    if (precomp) {
+      forEach(getLayers('all', precomp), function (layer) {
+        layer.remove();
+      });
+      precomp.name = name;
+      precomp.width = width;
+      precomp.height = height;
+      precomp.pixelAspect = pixel_aspect;
+      precomp.duration = duration;
+      precomp.frameRate = frame_rate;
+    } else {
+      precomp = app.project.items.addComp(name, width, height, pixel_aspect, duration, frame_rate);
+    }
+    layer.copyToComp(precomp);
+    
+    var copied_layer = precomp.layer(1);
+    removeComment(copied_layer, script_name);
+    copied_layer.enabled = true;
+    if (layer.inPoint < layer.outPoint) {
+      copied_layer.startTime = 0;
+    } else {
+      copied_layer.startTime = comp.duration;
+    }
+    setValue(copied_layer.transform.position, position, 0);
+    
+    return precomp;
+  }
 
   //Main
   var builder = new UIBuilder(global, 'LayerTrimmer', {
     version: '0.0.0',
     author: 'Kareobana',
-    url: 'http://atarabi.com/'
+    url: 'http://atarabi.com/',
+    titleWidth: 60,
   });
 
   builder
   .add('popup', 'Scan', {value: SCAN.ALL_FRAMES, items: getValues(SCAN)})
+  .add('checkbox', 'Extents', true)
   .add('popup', 'Center', {value: CENTER.CONTENTS, items: getValues(CENTER)})
   .add('number', 'Padding', {value: 0, minvalue: 0}, function () {
     this.set('Padding', ~~this.get('Padding'));
   })
   .add('script', 'Execute', function () {
-    function getRect(scan) {
-      var rect = {
-        left: Infinity,
-        top: Infinity,
-        right: -Infinity,
-        bottom: -Infinity,
-      };
-      
-      if (scan === SCAN.CURRENT_FRAME) {
-        var _rect = layer.sourceRectAtTime(layer.time, false);
-        rect.left = _rect.left;
-        rect.top = _rect.top;
-        rect.right = _rect.left + _rect.width;
-        rect.bottom = _rect.top + _rect.height;
-      } else if (scan === SCAN.ALL_FRAMES) {
-        var in_point = layer.inPoint,
-          out_point = layer.outPoint,
-          frame_duration = comp.frameDuration;
-          
-        if (in_point > out_point) {
-          var temp = in_point;
-          in_point = out_point;
-          out_point = temp;
-        }
-        
-        for (var t = in_point; t < out_point; t += frame_duration) {
-          var _rect = layer.sourceRectAtTime(t, false);
-          rect.left = Math.min(rect.left, _rect.left);
-          rect.top = Math.min(rect.top, _rect.top);
-          rect.right = Math.max(rect.right, _rect.left + _rect.width);
-          rect.bottom = Math.max(rect.bottom, _rect.top + _rect.height);
-        }
-      }
-      
-      return rect;
-    }
-    
-    function createPrecomp(name, rect, center, padding) {
-      var width,
-        height,
-        position,
-        pixel_aspect = comp.pixelAspect,
-        duration = Math.abs(layer.inPoint - layer.outPoint),
-        frame_rate = comp.frameRate;
-        
-      if (center === CENTER.COMPOSITION) {
-        width = 2 * (Math.ceil(Math.max(Math.abs(rect.left), Math.abs(rect.right))) + padding);
-        height = 2 * (Math.ceil(Math.max(Math.abs(rect.top), Math.abs(rect.bottom))) + padding);
-        position = [width / 2, height / 2];
-      } else if (center === CENTER.CONTENTS) {
-        width = Math.ceil(rect.right - rect.left) + 2 * padding;
-        height = Math.ceil(rect.bottom - rect.top) + 2 * padding;
-        position = [width / 2 - (rect.left + rect.right) / 2, height / 2 - (rect.top + rect.bottom) / 2];
-      }
-      
-      var precomp = app.project.items.addComp(name, width, height, pixel_aspect, duration, frame_rate);
-      layer.copyToComp(precomp);
-      
-      var copied_layer = precomp.layer(1);
-      if (layer.inPoint < layer.outPoint) {
-        copied_layer.startTime = 0;
-      } else {
-        copied_layer.startTime = comp.duration;
-      }
-      setValue(copied_layer.transform.position, position, 0);
-      
-      return precomp;
-    }
-    
     var layer = getSelectedLayer();
     if (!layer) {
       return alert('Select a layer.');
@@ -139,21 +213,54 @@
     }
     
     var comp = getActiveComp(),
-      rect = getRect(this.get('Scan'));
+      comment = getComment(layer)[this.getName()],
+      update = false;
       
-    var precomp_name = prompt('Input a composition\'s name', '');
-    if (!precomp_name) {
-      return;
-    } else if (getCompByName(precomp_name)) {
-      return alert(precomp_name + ' already exists.');
+    if (comment && comment.comp) {
+      update = true;
     }
     
-    var precomp = createPrecomp(precomp_name, rect, isTextLayer(layer) ? CENTER.CONTENTS : this.get('Center'), this.get('Padding')),
-      precomp_layer = comp.layers.add(precomp);
+    if (!update) {
+      var rect = getRect(comp, layer, this.get('Scan'), this.get('Extents'));
+      var precomp_name = prompt('Input a composition\'s name', '');
+      if (!precomp_name) {
+        return;
+      } else if (getCompByName(precomp_name)) {
+        removeComment(layer, this.getName());
+        return alert(precomp_name + ' already exists.');
+      }
       
-    precomp_layer.moveBefore(layer);
-    precomp_layer.startTime = Math.min(layer.inPoint, layer.outPoint);
-    layer.enabled = false;
+      var precomp = createPrecomp(this.getName(), comp, layer, precomp_name, rect, isTextLayer(layer) ? CENTER.CONTENTS : this.get('Center'), this.get('Padding')),
+        precomp_layer = comp.layers.add(precomp);
+      precomp_layer.moveBefore(layer);
+      precomp_layer.startTime = Math.min(layer.inPoint, layer.outPoint);
+  
+      comment = {};
+      comment[this.getName()] = {
+        comp: precomp_name
+      };
+      setComment(layer, comment);
+      
+      layer.enabled = false;
+    } else {
+      var precomp = getCompByName(comment.comp);
+      if (!precomp) {
+        return alert(comment.comp + ' is not found.');
+      }
+      var rect = getRect(comp, layer, this.get('Scan'), this.get('Extents'));
+      
+      createPrecomp(this.getName(), comp, layer, precomp.name, rect, isTextLayer(layer) ? CENTER.CONTENTS : this.get('Center'), this.get('Padding'), precomp);
+      
+      var precomp_layer = getLayer(function (layer) {
+        return isCompLayer(layer) && layer.source === precomp;
+      }, comp);
+      precomp_layer.moveBefore(layer);
+      precomp_layer.startTime = Math.min(layer.inPoint, layer.outPoint);
+      precomp_layer.inPoint = precomp_layer.startTime;
+      precomp_layer.outPoint = precomp_layer.startTime + precomp.duration;
+      
+      layer.enabled = false;
+    }
   })
   .build();
 
--- ScriptUI/LayerTrimmer/README.md
+++ ScriptUI/LayerTrimmer/README.md
@@ -8,14 +8,21 @@
 
 ### 基本
 
-1. **Scan**で現在のフレームの境界のみをチェックするか、全フレームの境界をチェックするかを決める。
+1. **Scan**で現在のフレームの境界のみをチェックするか、全フレームの境界をチェックするかを決める。また、シェイプレイヤーの場合は**Extents**でストローク等による描写範囲の拡大を考慮するかを決める。
 
-1. **Center**で、コンポジションの中心とコンテンツの中心のどちらを基準にするか決定する（テキストレイヤーはコンテンツ中心のみ）。
+1. **Center**で、コンポジションの中心とコンテンツの中心のどちらを基準にするか決める（テキストレイヤーはコンテンツ中心のみ）。
 
 1. **Execute**をクリックで実行。
 
+### 更新
+
+1. 更新したいシェイプレイヤーまたはテキストレイヤーを選択(一度実行したレイヤーにはコメントに情報が付加されるのでそれを参照)。
+
+1. **Execute**をクリックで更新。
+
 ## Dependencies
 
+- [KIKAKU.JSON](https://github.com/atarabi/AfterEffects-Scripts/tree/master/Startup/KikakuJSON)
 - [KIKAKU.Utils 1.0.0](https://github.com/atarabi/AfterEffects-Scripts/tree/master/Startup/KikakuUtils)
 - [KIKAKU.UIBuilder 2.0.0](https://github.com/atarabi/AfterEffects-Scripts/tree/master/Startup/KikakuUIBuilder)
 
